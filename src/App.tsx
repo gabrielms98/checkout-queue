b27b3ab998ba54448b097233c92619bc9eb52ae3
Gabriel Martins Silva <91923731+gabrielmsoptimalex@users.noreply.github.com>
2023-03-16 20:11:37 -0300

    feat: initial structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,51 @@
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const lines: string[] = ["a", "b", "c", "d", "e"];
+  const lineItems: number[][] = [
+    [1],
+    [4, 5, 6],
+    [7, 8, 9, 10],
+    [4, 5],
+    [7, 8, 9],
+  ];
+
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <div>
+        <input type="number" placeholder="Enter quantity"></input>
+        <button>Checkout</button>
+      </div>
+      <div className="flex gap-2 mt-2">
+        {lines &&
+          lines.map((line, i) => (
+            <div
+              className="border border-gray-300 rounded-md w-12 h-12 flex justify-center items-center"
+              key={i}
+            >
+              {line}
+            </div>
+          ))}
+      </div>
+      <div className="flex gap-2 mt-1">
+        {lineItems &&
+          lineItems.map((line) => {
+            return (
+              <div className="flex flex-col gap-1">
+                {line.map((item, i) => (
+                  <div
+                    className="border-2 border-gray-300 rounded-full w-12 h-12 flex justify-center items-center"
+                    key={i}
+                  >
+                    {item}
+                  </div>
+                ))}
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
