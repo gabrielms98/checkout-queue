64098ce3a9e8232513e83ceaa1e284ed527d4564
Gabriel Martins Silva <91923731+gabrielmsoptimalex@users.noreply.github.com>
2023-03-16 20:54:43 -0300

    feat: decrementing queue in a dumb way probably

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const lines: string[] = ["a", "b", "c", "d", "e"];
-  const lineItems: number[][] = [
+  const [lineItems, setLineItems] = useState<number[][]>([
     [1],
-    [4, 5, 6],
-    [7, 8, 9, 10],
-    [4, 5],
-    [7, 8, 9],
-  ];
+    [1, 5, 2],
+    [],
+    [],
+    [],
+  ]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const copy = [...lineItems];
+      for (let i = 0; i < copy.length; i++) {
+        if (!copy[i].length) continue;
+
+        copy[i][0]--;
+
+        if (copy[i][0] === 0) copy[i].shift();
+      }
+
+      setLineItems(copy);
+    }, 1000 * 2);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center">
@@ -29,17 +47,21 @@ function App() {
       </div>
       <div className="flex gap-2 mt-1">
         {lineItems &&
-          lineItems.map((line) => {
+          lineItems.map((line, j) => {
             return (
-              <div className="flex flex-col gap-1">
-                {line.map((item, i) => (
-                  <div
-                    className="border-2 border-gray-300 rounded-full w-12 h-12 flex justify-center items-center"
-                    key={i}
-                  >
-                    {item}
-                  </div>
-                ))}
+              <div className="flex flex-col gap-1" key={lines[j]}>
+                {line.length ? (
+                  line.map((item, i) => (
+                    <div
+                      className="border-2 border-gray-300 rounded-full w-12 h-12 flex justify-center items-center"
+                      key={i}
+                    >
+                      {item}
+                    </div>
+                  ))
+                ) : (
+                  <div className="border-2 border-gray-300 rounded-full w-12 h-12 flex justify-center items-center invisible"></div>
+                )}
               </div>
             );
           })}
